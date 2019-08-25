c0b670340d4ad9149cdb70122f1bd5b32e3d7e9b
Andrew Lord <andrewlord1990@gmail.com>
2019-08-25 11:56:56 +0100

    Tests for FileCommands
    Can only really unit test the symlink functions

---
+++ turtle/src/test/kotlin/com/lordcodes/turtle/FileCommandsTest.kt
@@ -0,0 +1,56 @@
+package com.lordcodes.turtle
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+internal class FileCommandsTest {
+    @TempDir
+    lateinit var temporaryFolder: File
+
+    private val shell by lazy { ShellScript(temporaryFolder) }
+    private val files: FileCommands by lazy { FileCommands(shell) }
+
+    @Test
+    fun createSymlink_fileArguments() {
+        val target = File(temporaryFolder, "targetFolder")
+        target.mkdir()
+
+        files.createSymlink(File("targetFolder"), File("linkedFrom"))
+
+        assertThat(shell.command("readlink", listOf("linkedFrom"))).isEqualTo("targetFolder")
+    }
+
+    @Test
+    fun createSymlink_stringArguments() {
+        val target = File(temporaryFolder, "targetFolder")
+        target.mkdir()
+
+        files.createSymlink("targetFolder", "linkedFrom")
+
+        assertThat(shell.command("readlink", listOf("linkedFrom"))).isEqualTo("targetFolder")
+    }
+
+    @Test
+    fun readSymlink_fileArguments() {
+        val target = File(temporaryFolder, "targetFolder")
+        target.mkdir()
+        shell.command("ln", listOf("-s", "targetFolder", "linkedFrom"))
+
+        val output = files.readSymlink(File("linkedFrom"))
+
+        assertThat(output).isEqualTo("targetFolder")
+    }
+
+    @Test
+    fun readSymlink_stringArguments() {
+        val target = File(temporaryFolder, "targetFolder")
+        target.mkdir()
+        shell.command("ln", listOf("-s", "targetFolder", "linkedFrom"))
+
+        val output = files.readSymlink("linkedFrom")
+
+        assertThat(output).isEqualTo("targetFolder")
+    }
+}
