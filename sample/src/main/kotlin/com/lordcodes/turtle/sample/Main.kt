93149d28086b966a77c7a7a0d84a9cc97f0e64fb
Andrew Lord <andrewlord1990@gmail.com>
2019-09-09 13:53:34 +0100

    Add example to sample that adds new Git command

--- sample/src/main/kotlin/com/lordcodes/turtle/sample/Main.kt
+++ sample/src/main/kotlin/com/lordcodes/turtle/sample/Main.kt
@@ -1,5 +1,6 @@
 package com.lordcodes.turtle.sample
 
+import com.lordcodes.turtle.GitCommands
 import com.lordcodes.turtle.shellRun
 
 /**
@@ -7,9 +8,13 @@ import com.lordcodes.turtle.shellRun
  */
 fun main() {
     shellRun {
+        println(git.gitAuthorName())
+
         command("mkdir", listOf("Test"))
         command("mkdir", listOf("Test2"))
         command("rm", listOf("-rf", "BLAH"))
         command("rm", listOf("-rf", "Test"))
     }
 }
+
+fun GitCommands.gitAuthorName() = gitCommand(listOf("--no-pager", "show", "-s", "--format=%an"))
