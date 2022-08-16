e048347a4a43a2306b7c0482aabd630386b18256
Jean-Michel Fayard <459464+jmfayard@users.noreply.github.com>
2022-08-16 20:05:05 +0200

    120: Command abstraction

---
+++ turtle/src/main/kotlin/com/lordcodes/turtle/commands/Command.kt
@@ -0,0 +1,49 @@
+package com.lordcodes.turtle.commands
+
+import java.io.File
+import java.net.URI
+import java.net.URL
+
+data class Command(
+    val list: List<String>
+) {
+    val command = list.firstOrNull()?.takeIf { it.isNotBlank() }
+        ?: error("Command.command is empty")
+
+    val args: List<String> = list.drop(1)
+}
+
+fun Command(vararg args: Any?): Command {
+    val mappedList = args.map { it.toArgument() }
+    val errors = mappedList.mapNotNull { it.exceptionOrNull() }
+    require(errors.isEmpty()) { "Command received invalid arguments: ${errors.map { it.message }}"}
+    val goodArguments = mappedList.mapNotNull { it.getOrNull() }
+    return Command(goodArguments)
+}
+
+interface HasCommandArgument {
+    val arg: String
+}
+
+data class ShortOption(val key: Char, val value: Any? = null): HasCommandArgument {
+    override val arg: String = when {
+        value == null -> "-$key"
+        value is String && value.isBlank() -> "-$key"
+        else -> "-$key=$value"
+    }
+}
+
+data class LongOption(val key: String, val value: Any? = null): HasCommandArgument {
+    override val arg: String = "$key=$value"
+}
+
+private fun Any?.toArgument(): Result<String?> = when {
+    this == null -> Result.success(null)
+    this is HasCommandArgument -> Result.success(arg)
+    this is String -> Result.success(this)
+    this is File -> Result.success(path)
+    this is Boolean || this is Int -> Result.success(toString())
+    this is URI || this is URL  -> Result.success(toString())
+    else -> Result.failure(IllegalArgumentException(this.toString()))
+}
+
---
+++ turtle/src/test/kotlin/com/lordcodes/turtle/commands/CommandTest.kt
@@ -0,0 +1,54 @@
+package com.lordcodes.turtle.commands
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.File
+import java.net.URL
+import kotlin.random.Random
+
+class CommandTest {
+
+    @Test
+    fun `Command works with Ints, Booleans, File, etc`() {
+        val dir = File(".")
+        val command = Command("command",
+            "--force", true,
+            "--directory", dir,
+            "--max-length", 1,
+            "--url", URL("http://example.com"),
+        )
+        val expected: List<String> = listOf("command",
+            "--force", "true", "--directory", ".",
+            "--max-length", "1", "--url", "http://example.com",
+        )
+        assertIterableEquals(expected, command.list)
+    }
+
+    @Test
+    fun `command support short and long options`() {
+        val dir = File(".")
+        val command = Command("command",
+            LongOption("--force", true),
+            LongOption("--directory", dir),
+            ShortOption('f'),
+            ShortOption('o', "output.txt"),
+        )
+        val expected: List<String> = listOf("command",
+            "--force=true",
+            "--directory=.",
+            "-f",
+            "-o=output.txt",
+        )
+        assertIterableEquals(expected, command.list)
+    }
+
+    @Test
+    fun `Test for invalid arguments`() {
+        val invalidArgument = Random(42)
+        val e = assertThrows<IllegalArgumentException> {
+            Command("command", invalidArgument)
+        }
+        assertEquals("Command received invalid arguments: [$invalidArgument]", e.message)
+    }
+}
