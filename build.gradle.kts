25c556248159c3729e9c757e58045405fb7aabce
Andrew Lord <andrew@lordcodes.com>
2023-07-23 18:23:26 +0100

    Update Ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
     kotlin("jvm") version "1.8.21" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.0"
     id("com.github.ben-manes.versions") version "0.47.0"
-    id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
-    id("org.jlleitschuh.gradle.ktlint-idea") version "11.4.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "11.5.0"
 }
 
 allprojects {
