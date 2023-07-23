d378cc2bd93b56509f38f5375d057a3785dfa014
Andrew Lord <andrew@lordcodes.com>
2023-07-23 18:25:22 +0100

    Update Kotlin to 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 
 plugins {
     base
-    kotlin("jvm") version "1.8.21" apply false
+    kotlin("jvm") version "1.9.0" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.0"
     id("com.github.ben-manes.versions") version "0.47.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
