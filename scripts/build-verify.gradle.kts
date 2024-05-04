69c26257ec1f7a0de8b6534148fe5b063fc91674
Andrew Lord <andrew@lordcodes.com>
2024-05-04 12:33:11 +0100

    Update custom format/check commands

--- scripts/build-verify.gradle.kts
+++ scripts/build-verify.gradle.kts
@@ -4,16 +4,16 @@ tasks {
 }
 
 fun TaskContainerScope.runCodeFormatting() {
-    val tasks = rootProject.getTasksByName("ktlintFormat", true)
-    register("lcCodeFormat") {
+    val tasks = rootProject.getTasksByName("formatKotlin", true)
+    register("lcformat") {
         dependsOn(tasks)
         group = "lordcodes"
     }
 }
 
 fun TaskContainerScope.runChecks() {
-    val tasks = rootProject.getTasksByName("ktlintCheck", true)
-    register("lcChecks") {
+    val tasks = rootProject.getTasksByName("lintKotlin", true)
+    register("lcchecks") {
         dependsOn(tasks, "detekt")
         group = "lordcodes"
     }
